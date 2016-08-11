cae95fe62230ed6a99cde4428f38e40a1e687c08
JB <jb@codecorsair.com>
2016-08-11 12:15:57 -0400

    mapping emojis -- a little cleaner sass & ts for emoji parsing making it simpler to add new emojis

--- widgets/cu-xmpp-chat/src/components/ParseEmoji.tsx
+++ widgets/cu-xmpp-chat/src/components/ParseEmoji.tsx
@@ -6,137 +6,121 @@
 
 import * as React from 'react';
 
+enum emojis {
+  ANGRY,
+  BAFFLED,
+  CONFUSED,
+  COOL,
+  CRYING,
+  EVIL,
+  FRUSTRATED,
+  GRIN,
+  HAPPY,
+  HIPSTER,
+  NEUTRAL,
+  SAD,
+  SHOCKED,
+  SLEEPY,
+  SMILE,
+  TONGUE,
+  WINK,
+  WONDERING,
+  ANDREWFEZ,
+  ANDREW,
+  JBHEAD,
+  JB,
+  MARKHEAD,
+  MJHEAD,
+  MJ,
+  MJELF,
+  SANDRAGRIN,
+  TIMHEAD,
+  TINYMICHELLE,
+  BATGRIN,
+  BATHAPPY,
+  BATMAD,
+  BATSAD,
+  BATTONGUE,
+  BOB,
+  BOBLOVE,
+  CROWN,
+  CUPOTEARS,
+  QQCUP,
+  DRAGONLICK,
+  DRAGONWHALE,
+  DUCK,
+  DUCKMAGE,
+  FROWNSTRM,
+  SADSTRM,
+  HAPPYSTRM,
+  JUDGINGDUCKONE,
+  JUDGINGDUCK1,
+  JUDGINGDUCKTWO,
+  JUDGINGDUCK2,
+  ARTHURIANLOVE,
+  ARTLOVE,
+  TUATHALOVE,
+  TDDLOVE,
+  VIKINGLOVE,
+  VIKLOVE,
+  PLUSTEN,
+  PLUS10,
+  PVP,
+  RAGE,
+  SALT,
+  ARTHURIANSHIELD,
+  ARTSHIELD,
+  TUATHASHIELD,
+  TDDSHIELD,
+  VIKINGSHIELD,
+  VIKSHIELD,
+  SHOTSFIRED,
+  UNICORN,
+}
+
+const emojiNames = Object.keys(emojis).map(k => (emojis as any)[k]).filter(v => typeof v === 'string') as string[];
+
 function emojiNameFromText(text:string): string {
-  //case insensitive commands
-  switch(text.toLowerCase()) {
-    case ':angry:':
-      return 'angry';
-    case ':baffled:': case 'o.o':
-      return 'baffled';
-    case ':confused:':
-      return 'confused';
-    case ':cool:': case '8)': case '8-)':
-      return 'cool';
-    case ':cry:': case ':\'(': case ':qq:':
-      return 'crying';
-    case ':evil:': case '3:)':
-      return 'evil';
-    case ':frustrated:':
-      return 'frustrated';
-    case ':grin:':
-      return 'grin';
-    case ':happy:': case ':)': case ':-)':
-      return 'happy';
-    case ':hipster:':
-      return 'hipster';
-    case ':neutral:': case ':|': case ':-|':
-      return 'neutral';
-    case ':sad:': case ':(': case ':-(':
-      return 'sad';
-    case ':shocked:': case '8o':
-    case '8-o': case ':o':
-      return 'shocked';
-    case ':sleepy:': case ':zzz:':
-      return 'sleepy';
-    case ':smile:': case ':>':
-      return 'smile';
-    case ':tongue:':
-    case ':p': case ':-p':
-    case ';p': case ';-p':
-    case '8p': case '8-p':
-      return 'tongue';
-    case ':wink:': case ';)': case ';-)':
-      return 'wink';
-    case ':wondering:': case ':wonder:':
-      return 'wondering';
-    //add CSE custom emoticons
-    case ':andrewfez:':
-      return 'andrewfez';
-    case ':jbhead:': case ':jb:':
-      return 'jbhead';
-    case ':markhead:': case ':mj:': case ':mark:':
-      return 'markhead';
-    case ':mjelf:':
-      return 'mjelf';
-    case ':sandragrin:':
-      return 'sandragrin';
-    case ':timhead:':
-      return 'timhead';
-    case ':tinymichelle:':
-      return 'tinymichelle';
-    case ':batgrin:':
-      return 'batgrin';
-    case ':bathappy:':
-      return 'bathappy';
-    case ':batmad:':
-      return 'batmad';
-    case ':batsad:':
-      return 'batsad';
-    case ':battongue:':
-      return 'battongue';
-    case ':bob:':
-      return 'bob';
-    case ':boblove:':
-      return 'boblove';
-    case ':crown:':
-      return 'crown';
-    case ':cupotears:': case ':qqcup:':
-      return 'cupotears';
-    case ':dragonlick:':
-      return 'dragonlick';
-    case ':dragonwhale:':
-      return 'dragonwhale';
-    case ':duck:':
-      return 'duck';
-    case ':duckmage:':
-      return 'duckmage';
-    case ':frownstrm:': case ':sadstrm:':
-      return 'frownstrm';
-    case ':happystrm:':
-      return 'happystrm';
-    case ':judgingduckone:': case ':judgingduck1:':
-      return 'judgingduckone';
-    case ':judgingducktwo:': case ':judgingduck2:':
-      return 'judgingducktwo';
-    case ':arthurianlove:': case ':artlove:':
-      return 'arthurianlove';
-    case ':tuathalove:': case ':tddlove:':
-      return 'tuathalove';
-    case ':vikinglove:': case ':viklove:':
-      return 'vikinglove';
-    case ':plusten:': case ':plus10:':
-      return 'plusten';
-    case ':pvp:':
-      return 'pvp';
-    case ':rage:':
-      return 'rage';
-    case ':salt:':
-      return 'salt';
-    case ':arthurianshield:': case ':artshield:':
-      return 'arthurianshield';
-    case ':tuathashield:': case ':tddshield:':
-      return 'tuathashield';
-    case ':vikingshield:': case ':vikshield:':
-      return 'vikingshield';
-    case ':shotsfired:':
-      return 'shotsfired';
-    case ':unicorn:':
-      return 'unicorn';
-  }
-  //case sensitive commands
+
+  // parse symbol emoji -- like :D 
   switch(text) {
     case ':D': case ':-D':
-      return 'grin';
-    case '-_-zzZ': case '(-_-)zzZ':
-      return 'sleepy';
+      return 'GRIN';
+    case '-_-zzZ': case '(-_-)zzZ': case ':zzz:':
+      return 'SLEEPY';
+    case '8-o': case ':o': case '8o':
+      return 'SHOCKED';
+    case '8)': case '8-)':
+      return 'COOL';
+    case ':\'(': case ':qq:':
+      return 'CRYING';
+    case ':)': case ':-)':
+      return 'HAPPY';
+    case ':|': case ':-|':
+      return 'NEUTRAL';
+     case ':(': case ':-(':
+      return 'SAD';
+    case ':p': case ':-p': case ';p':
+    case ';-p': case '8p': case '8-p':
+      return 'TONGUE';
+    case ':>':
+      return 'SMILE';
+    case 'o.o':
+      return 'BAFFLED';
+    case '3:)':
+      return 'EVIL';
+    case ';)': case ';-)':
+      return 'WINK';
   }
-  return null;
+
+  var upper = text.replace(':', '').toUpperCase();
+  return emojiNames.findIndex(n => n === upper) ? upper : null;
 }
 
 function fromText(text: string, keygen: () => number) : JSX.Element[] {
   const emoji : string = emojiNameFromText(text);
   if (emoji) {
-    return [<span key={keygen()} className={'chat-emoticon emote-' + emoji}></span>];
+    return [<span key={keygen()} className={'emoji emoji--' + emoji}></span>];
   }
 }
 
