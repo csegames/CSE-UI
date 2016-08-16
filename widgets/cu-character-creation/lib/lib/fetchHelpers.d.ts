be9262e21faecd5fe98f38808eacd0e3b48599bb
Brian Bothwell <sysrage@gmail.com>
2016-08-15 21:44:58 -0400

    Fix z-index for 'Character Name' in cu-character-creation.

--- widgets/cu-character-creation/lib/lib/fetchHelpers.d.ts
+++
@@ -1,4 +0,0 @@
-import 'isomorphic-fetch';
-export declare function checkStatus(response: any): any;
-export declare function parseJSON(response: any): any;
-export declare function fetchJSON(url: string, options?: any): any;
