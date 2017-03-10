6fb5f120cb12fd6a321e57a4b2dbced1556196d3
JB <jb@codecorsair.com>
2017-03-10 16:32:44 -0500

    fix a patcher bug for setting api hostname

--- patcher/src/widgets/Controller/components/PatchButton/index.tsx
+++ patcher/src/widgets/Controller/components/PatchButton/index.tsx
@@ -122,9 +122,9 @@ class PatchButton extends React.Component<PatchButtonProps, PatchButtonState> {
     let launchString = this.commands.toLowerCase();
     if (selectedCharacter && selectedCharacter.id !== '' && selectedServer.channelID != 27) {
       if (!launchString.includes('servershardid') && !launchString.includes('server')) launchString += ` servershardid=${selectedServer.shardID}`;
-      if (!launchString.includes('character=') || !launchString.includes('character =')) launchString += ` character=${selectedCharacter.id}`;
+      if (!launchString.includes('character=') && !launchString.includes('character =')) launchString += ` character=${selectedCharacter.id}`;
       const apiHost = selectedServer.apiHost || 'https://api.camelotunchained.com';
-      if (!launchString.includes('webapihost=') || !launchString.includes('webapihost =')) launchString += ` webapihost=${apiHost}`;
+      if (!launchString.includes('webapihost=') && !launchString.includes('webapihost =')) launchString += ` webapihost=${apiHost}`;
       launchString += ' autoconnect=1';
     }
     patcher.launchChannelfunction(selectedServer.channelID | 0, launchString);
