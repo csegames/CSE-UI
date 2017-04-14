b6f8ad8e1df87d5ca7760f6148feab1a8e49231d
JB <jb@codecorsair.com>
2017-04-14 12:55:25 -0400

    fixup patcher build

--- patcher/package-scripts.js
+++ patcher/package-scripts.js
@@ -14,18 +14,13 @@ module.exports = {
     },
     babel: 'babel tmp -d tmpp',
     browserify: {
-      default: 'browserify tmpp/index.js -o dist/ui/js/%npm_package_config_name%.js --fast --noparse=FILE -u camelot-unchained -u es6-promise -u eventemitter3 -u isomorphic-fetch -u moment -u node-xmpp-client -u normalizr -u react -u react-addons-css-transition-group -u react-dom  -u react-uedux -u react-tap-event-plugin -u redux -u redux-thunk -u reflux',
-      lib: 'browserify -r camelot-unchained -r es6-promise -r eventemitter3 -r isomorphic-fetch -r moment -r node-xmpp-client -r normalizr -r react -r react-addons-css-transition-group -r react-dom  -r react-redux -r react-tap-event-plugin -r redux -r redux-thunk -r reflux > dist/ui/js/lib.js'
-    },
-    prebrowserify: {
-      lib: 'mkdirp dist/ui/js'
+      default: 'mkdirp dist/ui/js && browserify tmpp/index.js -o dist/ui/js/%npm_package_config_name%.js --fast --noparse=FILE -u camelot-unchained -u es6-promise -u eventemitter3 -u isomorphic-fetch -u moment -u node-xmpp-client -u normalizr -u react -u react-addons-css-transition-group -u react-dom  -u react-redux -u react-tap-event-plugin -u redux -u redux-thunk -u reflux',
+      lib: 'mkdirp dist/ui/js && browserify -r camelot-unchained -r es6-promise -r eventemitter3 -r isomorphic-fetch -r moment -r node-xmpp-client -r normalizr -r react -r react-addons-css-transition-group -r react-dom  -r react-redux -r react-tap-event-plugin -r redux -r redux-thunk -r reflux > dist/ui/js/lib.js'
     },
     build: {
       sass: 'node-sass src -o dist/ui/css --importer node_modules/sass-importer-node/sass-importer-node.js',
-      default: 'nps lint && nps build.sass && nps copy.misc -s && tsc && nps babel && nps browserify'
+      default: 'rimraf tmp && rimraf tmpp && rimraf dist/ui && nps browserify.lib && nps lint && nps build.sass && nps copy.misc -s && tsc && nps babel && nps browserify && rimraf tmp && rimraf tmpp'
     },
-    postbuild: 'rimraf tmp && rimraf tmpp',
-    prebuild: 'rimraf tmp && rimraf tmpp && rimraf dist/ui && nps browserify.lib',
     publish: 'nps build',
     serve: 'http-server -p 9000 dist/ui/',
     debug: 'nps build && nps copy.patcher && nps patcher',
