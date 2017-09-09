0620ebc9e9cb16692c0fdd0385a6dba7ff25849f
Justin Vick <justin.zezmin@gmail.com>
2017-09-09 18:11:28 -0500

    changed filterEditMenu height to 600, 215 was cramped and felt too small.

--- game/hud/src/widgets/Character/components/Inventory/components/FilterSelectMenu.tsx
+++ game/hud/src/widgets/Character/components/Inventory/components/FilterSelectMenu.tsx
@@ -29,7 +29,7 @@ export interface FilterSelectMenuStyle extends StyleDeclaration {
 
 const containerDimensions = {
   width: 300,
-  height: 215,
+  height: 600,
 };
 
 export const defaultFilterSelectMenuStyle: FilterSelectMenuStyle = {
