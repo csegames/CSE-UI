4ab4cd829d47aa424952abf95c5bf6d34df861af
Justin Vick <justin.zezmin@gmail.com>
2017-09-09 21:07:59 -0500

    realized there was a typo in my file creation header

--- game/hud/src/widgets/Map/components/MapMain.tsx
+++ game/hud/src/widgets/Map/components/MapMain.tsx
@@ -3,9 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * @Author: Justin Vick (jusitn.zezmin@gmail.com)
+ * @Author: Justin Vick (justin.zezmin@gmail.com)
  * @Date: 9/8/2017 16:48
- * @Last Modified by: Justin Vick (jusitn.zezmin@gmail.com)
+ * @Last Modified by: Justin Vick (justin.zezmin@gmail.com)
  * @Last Modified time: 9/8/2017 16:48
  */
 
