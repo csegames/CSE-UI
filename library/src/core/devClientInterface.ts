152b6f009262f869eae19661610b05daeb825270
JB <jb@codecorsair.com>
2018-06-12 14:02:00 -0400

    Update Library 0.1.33 -- Add EntityID Targetting to client interface

--- library/src/core/devClientInterface.ts
+++ library/src/core/devClientInterface.ts
@@ -440,9 +440,12 @@ const devClientInterface: clientInterface = {
 
   CancelItemPlacement: (): void => {},
 
-  SendCommitItemRequest: (
-    callback: (itemINstanceIDString: string, position: any, rotation: any, scale: any) => void
-  ): void => {},
+  SendCommitItemRequest: (callback: (itemINstanceIDString: string, position: any, rotation: any, scale: any) => void)
+  : void => {},
+
+  /* Target */
+  RequestFriendlyTargetEntityID: (entityID:string): void => {},
+  RequestEnemyTargetEntityID: (entityID:string): void => {},
 
   apiVersion: 1,
   characterID: 'AABBCCDDEEFFGG',
