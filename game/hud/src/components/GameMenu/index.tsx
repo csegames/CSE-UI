05d3da70745910c56561fbce59f9f8c856967ec4
Lars Dunker <itdesign@gmail.com>
2018-06-24 12:27:12 +0200

    Redesign game menu

--- game/hud/src/components/GameMenu/index.tsx
+++ game/hud/src/components/GameMenu/index.tsx
@@ -5,8 +5,8 @@
  */
 
 import * as React from 'react';
-import { client, events, utils } from '@csegames/camelot-unchained';
-import styled, { css } from 'react-emotion';
+import { client, events } from '@csegames/camelot-unchained';
+import styled from 'react-emotion';
 
 export interface GameMenuStyle {
   GameMenu: React.CSSProperties;
@@ -17,54 +17,145 @@ export interface GameMenuStyle {
   closeButton: React.CSSProperties;
 }
 
-const Container = styled('div')`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+const OuterContainer = styled('div')`
+  position: relative;
+`;
+
+const Container = styled('div')` {
+  position: relative;
   pointer-events: all;
-  width: 300px;
+  width: 320px;
   height: 200px;
-  background-color: rgba(0, 0, 0, 0.5);
-  border: 1px solid ${utils.lightenColor('#202020', 30)};
+  padding: 0px;
+  margin:0 auto;
+  background-color: gray;
+  color: white;
+  background: url(images/gamemenu/gamemenu-bg-grey.png) no-repeat;
+  z-index: 1;
+  border: 1px solid #6e6c6c;
+  box-shadow: 0 0 30px 0 #000;
 `;
 
-const Header = styled('div')`
-  padding: 5px 0;
+const MenuTitle = styled('div')` {
   text-align: center;
-  background-color: #202020;
-  color: white;
-  border-bottom: 1px solid ${utils.lightenColor('#202020', 30)};
+  background: url(images/gamemenu/gamemenu-top-title.png) center top no-repeat;
+  margin: 0 auto -9px auto;
+  position: relative;
+  z-index: 999;
+  width: 319px;
+  height: 23px;
+  h6 {
+    color: #848484;
+    font-size: 10px;
+    text-transform: uppercase;
+    padding: 7px 0 0 0;
+    margin: 0 0 0 0;
+    font-family: 'Caudex', serif;
+  }
 `;
 
-const Body = styled('div')`
-  padding: 5px;
-  flex: 1;
+const MenuCorner = styled('div')` {
+  position: absolute;
+  min-width: 320px;
+  min-height: 200px;
+  background:
+  url(images/gamemenu/gamemenu-ornament-top-left.png) left 0 top 0 no-repeat,
+  url(images/gamemenu/gamemenu-ornament-top-right.png) right 0 top 0 no-repeat,
+  url(images/gamemenu/gamemenu-ornament-bottom-left.png) left 0 bottom 0 no-repeat,
+  url(images/gamemenu/gamemenu-ornament-bottom-right.png) right 0 bottom 0 no-repeat;
+  z-index: 1;
+`;
+
+const MenuContent = styled('div')` {
+  height: 115px;
+  margin-top: 30px;
+  max-height: 345px;
+  padding: 10px 20px;
+  box-sizing: border-box;
+  overflow: hidden;
+  z-index: 99;
+  position: absolute;
+  width: 100%;
+  width: calc(100% - 40px);
+`;
+const MenuButton = styled('div')`
+  background: url(images/gamemenu/button-big-off.png) no-repeat;
+  width: 200px;
+  height: 30px;;
+  border: none;
+  margin: 12px 16px 0 16px;
+  cursor: pointer;
+  color: #848484;
+  font-family: 'Caudex', serif;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin: 0 auto 20px;
+  display: block;
+  line-height: 30px;
+  text-align: center;
+  &:hover {
+    color: #968876;
+    background: url(images/gamemenu/button-big-on.png) no-repeat;
+  }
+`;
+
+const MenuFooter = styled('div')` {
+  position: absolute;
+  min-width: 320px;
+  height: 55px;
+  bottom: 0;
+  left: 0;
+  background: rgba(55, 52, 51, 0.3);
+  border-top: 1px solid #3b3634;
+`;
+
+const MenuFooterButton = styled('div')`
+  background: url(images/gamemenu/button-off.png) no-repeat;
+  width: 95px;
+  height: 30px;;
+  border: none;
+  margin: 12px 16px 0 16px;
+  cursor: pointer;
+  color: #848484;
+  font-family: 'Caudex', serif;
+  font-size: 12px;
+  text-transform: uppercase;
+  line-height: 30px;
+  text-align: center;
+  &:hover {
+    color: #968876;
+    background: url(images/gamemenu/button-on.png) no-repeat;
+  }
+`;
+
+const MenuFooterBorder = styled('div')` {
+  position: absolute;
+  border: 1px solid #2e2b28;
+  margin: 7px 10px 0;
+  display: block;
+  width: 300px;
+  height: 40px;
+  z-index: 3;
+`;
+
+const MenuButtonOuter = styled('div')` {
+  position: absolute;
+  width: 100%;
+  z-index: 4;
   display: flex;
-  flex-direction: column;
   justify-content: center;
 `;
 
-const Footer = styled('div')`
-  display: flex;
-  justify-content: center;
-  padding: 5px 0;
-  background-color: #202020;
-  color: white;
-  border-top: 1px solid ${utils.lightenColor('#202020', 30)}
+const MenuButtonLeft = styled('div')` {
+  background: url(images/gamemenu/gamemenu-botnav-left-ornament.png) no-repeat;
+  height: 55px;
+  width: 75px
 `;
 
-const MenuButton = styled('button')`
-  margin: 0;
-  height: 30px;
-  font-size: 17px;
-`;
-
-const OptionsButton = css`
-  margin: 0 0 5px 0;
-`;
-
-const CloseButton = styled('button')`
-  margin: 0;
+const MenuButtonRight = styled('div')` {
+  background: url(images/gamemenu/gamemenu-botnav-right-ornament.png) no-repeat;
+  height: 55px;
+  width: 75px
 `;
 
 export const GameMenuDimensions = {
@@ -90,22 +181,35 @@ export class GameMenu extends React.Component<GameMenuProps, GameMenuState> {
 
   public render() {
     return this.state.visible ? (
-      <Container>
-        <Header>
-          MENU
-        </Header>
-        <Body>
-          <MenuButton
-            className={OptionsButton}
-            onClick={this.onOptionsClick}>
-            SETTINGS
-          </MenuButton>
-          <MenuButton onClick={this.onQuitGameClick}>QUIT GAME</MenuButton>
-        </Body>
-        <Footer>
-          <CloseButton onClick={this.fireVisibilityEvent}>CLOSE MENU</CloseButton>
-        </Footer>
-      </Container>
+      <OuterContainer>
+        <MenuTitle><h6>Menu</h6></MenuTitle>
+        <MenuCorner />
+        <Container>
+          <MenuCorner />
+          <MenuContent>
+              <MenuButton
+                  onClick={this.onOptionsClick}>
+                  Settings
+              </MenuButton>
+              <MenuButton
+                  onClick={this.onQuitGameClick}>
+                  Quit game
+              </MenuButton>
+          </MenuContent>
+          <MenuFooter>
+              <MenuFooterBorder />
+              <MenuButtonOuter>
+                  <MenuButtonLeft />
+                    <MenuFooterButton
+                      onClick={this.fireVisibilityEvent}>
+                      Close menu
+                    </MenuFooterButton>
+                  <MenuButtonRight />
+              </MenuButtonOuter>
+          </MenuFooter>
+        </Container>
+      </OuterContainer>
+
     ) : null;
   }
 
