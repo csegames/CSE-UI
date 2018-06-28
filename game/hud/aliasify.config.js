976b059931d496f47d781c7ae9ba1b4e151b4611
Mehuge <adf@sorcerer.co.uk>
2018-06-28 21:40:49 +0100

    Add aliasify config despite .gitignores objection!

---
+++ game/hud/aliasify.config.js
@@ -0,0 +1,11 @@
+var path = require("path");
+module.exports = {
+    replacements: {
+        "^UI/(\\w+)": ".\\tmpp\\components\\UI\\$1",
+        "^lib/(\\w+)": ".\\tmpp\\lib\\$1",
+        "^services/(\\w+)": ".\\tmpp\\services\\$1",
+        "^widgets/(\\w+)": ".\\tmpp\\widgets\\$1",
+        "^components/(\\w+)": ".\\tmpp\\components\\$1",
+    },
+    verbose: false
+};
