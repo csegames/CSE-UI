b5714555c449891a41e38af61993279b38e7c3de
JB <jb@codecorsair.com>
2018-09-28 18:13:04 -0400

    fixup library index

--- library/src/index.ts
+++ library/src/index.ts
@@ -20,46 +20,11 @@ export {
   webAPI,
 };
 
-
 // ------------------------------------------------------ //
 // ------------------------------------------------------ //
 // OLD STUFF - TODO: Audit and update / remove excess     //
 // ------------------------------------------------------ //
 // ------------------------------------------------------ //
-
-
-// core
-import CoreSettings from './core/CoreSettings';
-import clientInterface from './core/clientInterface';
-import client from './core/client';
-
-// constants
-import abilityTags from './core/constants/abilityConstants/abilityTags';
-import buildUIMode from './core/constants/buildUIMode';
-import channelId from './core/constants/channelId';
-import emotes from './core/constants/emotes';
-import soundEvents from './core/constants/soundEvents';
-import tagConstraintType from './core/constants/tagConstraintType';
-import gearSlot from './core/constants/gearSlot';
-import plotPermissions from './core/constants/plotPermissions';
-import attributeType from './core/constants/attributeType';
-import warbandRoles from './core/constants/warbandRoles';
-import warbandRanks from './core/constants/warbandRanks';
-import warbandPermissions from './core/constants/warbandPermissions';
-
-// libraries
-import * as core from './core/core';
-
-export * from './core/core';
-
-
-import * as restAPI from './restapi';
-
-export * from './webAPI/definitions';
-
-export * from './groups';
-
-export * from './building';
 
 import * as signalr from './signalR';
 
@@ -79,59 +44,13 @@ import * as components from './components';
 export * from './components';
 
 export default {
-
-  // core
-  CoreSettings,
-  client,
-
-  // core constants - #TODO: remove these (shouldn't be using them)
-  abilityTags,
-  buildUIMode,
-  channelId,
-  emotes,
-  soundEvents,
-  tagConstraintType,
-  gearSlot,
-  plotPermissions,
-  attributeType,
-  warbandRoles,
-  warbandRanks,
-  warbandPermissions,
-
   components,
-
   ...slashCommandsExports,
-
 };
 
 export {
-
-  restAPI,
-
   // cu
   utils,
-
-  // core
-  CoreSettings,
-  clientInterface,
-  client,
-
-  // core constants
-  abilityTags,
-  buildUIMode,
-  channelId,
-  emotes,
-  soundEvents,
-  tagConstraintType,
-  gearSlot,
-  plotPermissions,
-  attributeType,
-  warbandRanks,
-  warbandRoles,
-  warbandPermissions,
-
-  // libraries
-  core,
 
   // misc
   signalr,
