a30343234f8c2daecb9bed05296eb40596dfb058
JB <jb@codecorsair.com>
2018-11-26 17:15:29 -0500

    log to console a warning when attempting to use cuAPI methods that haven't been overriden

--- library/src/game/cuAPIShim.ts
+++ library/src/game/cuAPIShim.ts
@@ -7,10 +7,7 @@
 function cuAPIShim() {
   return {
     initialized: true,
-    OnInitialized: (c: () => void): number => {
-      c();
-      return -1;
-    },
+
     /* Client Options */
     muteVolume: false,
     mainVolume: 1,
@@ -48,330 +45,8 @@ function cuAPIShim() {
       blood: { current: 500, max: 1000 },
     },
 
-    FOV: (degrees: number) => {
-    },
-    DropLight: (intensity: number, radius: number, red: number, green: number, blue: number) => {
-    },
-    ResetLights: () => {
-    },
-    RemoveLight: () => {
-    },
-
-    OnServerConnected: (c: (isConnected: boolean) => void): number => {
-      c(false);
-      return -1;
-    },
-    PlaySoundEvent: (id: number): void => {
-    },
-    ToggleCamera: (): void => {
-    },
-    ReloadUI: (name: string): void => {
-      game.reloadUI();
-    },
-    ReloadAllUI: (): void => {
-      game.reloadUI();
-    },
-    OpenUI: (name: string): void => {
-    },
-    CloseUI: (name: string): void => {
-    },
-    HideUI: (name: string): void => {
-    },
-    ShowUI: (name: string): void => {
-    },
-    ToggleUIVisibility: (name: string): void => {
-    },
-    RequestInputOwnership: (): void => {
-    },
-    ReleaseInputOwnership: (): void => {
-    },
-    Quit: (): void => {
-    },
-    CrashTheGame: (): void => {
-    },
-    OnOpenUI: (callback: (name: string) => void): void => {
-    },
-    OnCloseUI: (callback: (name: string) => void): void => {
-    },
-    OnShowUI: (callback: (name: string) => void): void => {
-    },
-    OnHideUI: (callback: (name: string) => void): void => {
-    },
-
-    /* Respawn */
-    Respawn: (id: string): void => {
-      game.selfPlayerState.respawn(id + '');
-    },
-
-    /* Skills */
-
-    OnSkillStateChanged: (callback: (skillState: any) => void): void => {},
-
-    OnSkillBarChanged: (callback: (newSkillBar: any[]) => void): void => {},
-
-    /* Abilities */
-
-    Attack: (abilityID: string): void => {
-    },
-
-    OnAbilityError: (c: (message: string) => void): void => {
-    },
-
-    /* HUD */
-    OnToggleHUDItem: (c: (name: string) => void): void => {
-    },
-
-    /* Equipped Gear */
-
-    SubscribeGear: (subscribe: boolean): void => {
-    },
-    OnGearAdded: (callback: (item: any) => void): void => {
-    },
-    OnGearRemoved: (callback: (itemID: string) => void): void => {
-    },
-
-    UnequipItem: (itemID: string): void => {
-    },
-
-    /* Inventory */
-
-    SubscribeInventory: (subscribe: boolean): void => {
-    },
-    OnInventoryAdded: (callback: (item: any) => void): void => {
-    },
-    OnInventoryRemoved: (callback: (itemID: string) => void): void => {
-    },
-
-    EquipItem: (itemID: string): void => {
-    },
-
-    DropItem: (itemID: string): void => {
-    },
-
-    /* Config */
-
-    OnReceiveConfigVars: (c: (configs: string) => void): void => {
-    },
-    OnReceiveConfigVar: (c: (config: any) => void): void => {
-    },
-    OnConfigVarChanged: (c: (isChangeSuccessful: boolean) => void): void => {
-    },
-    SaveConfigChanges: (): void => {
-    },
-    OnSavedConfigChanges: (c: () => void): void => {
-    },
-    RestoreConfigDefaults: (tag: any): void => {
-    },
-    ChangeConfigVar: (variable: string, value: string): void => {
-    },
-    CancelChangeConfig: (variable: string): void => {
-    },
-    CancelAllConfigChanges: (tag: any): void => {
-    },
-    GetConfigVars: (tag: any): void => {
-    },
-    GetConfigVar: (variable: string): void => {
-    },
-    RequestDisplayModes: (): void => {
-    },
-    OnDisplayModesChanged: (c: (displayModes: any[]) => void): void => {
-    },
-    SetDisplayMode: (wantFullScreen: boolean, width: number, height: number): void => {
-    },
-
-    /* Building | CUBE */
-    OnBuildingModeChanged: (c: (buildingMode: number) => void): void => {
-    },
-
-    // responds with all blocks -- triggered by a call to 'RequestBlocks'
-    OnReceiveBlocks: (c: (buildingDict: any) => void): void => {
-    },
-
-    // responds with all substance ids -- triggered by a call to 'RequestSubstances'
-    OnReceiveSubstances: (c: (substances: any) => void): void => {
-    },
-
-    // responds with block ids for a specific substance last passed into 'BlockIDsforSubstanceID'
-    OnReceiveBlockIDs: (c: (blockIds: [number]) => void): void => {
-    },
-
-    OnReceiveBlockTags: (c: (blockID: number, tagDict: any) => void): void => {
-    },
-    OnReceiveScreenShot: (c: (screenShotString: any) => void): void => {
-    },
-    OnCopyBlueprint: (c: () => void): void => {
-    },
-    OnNewBlueprint: (c: (index: number, name: string) => void): void => {
-    },
-    OnDownloadBlueprints: (c: (charId: string) => void): void => {
-    },
-    OnUploadBlueprint: (c: (charId: string, name: string, data: any) => void): void => {
-    },
-    OnBlueprintSelected: (c: () => void): void => {
-    },
-    OnBlockSelected: (c: (blockID: number) => void): void => {
-    },
-
-
-    ToggleBuildingMode: (): void => {
-    },
-    SetBuildingMode: (newMode: number): void => {
-    },
-    UndoCube: (): void => {
-    },
-    RedoCube: (): void => {
-    },
-    CommitBlock: (): void => {
-    },
-    CancelBlockPlacement: (): void => {
-    },
-    BlockRotateX: (): void => {
-    },
-    BlockRotateY: (): void => {
-    },
-    BlockRotateZ: (): void => {
-    },
-    BlockFlipX: (): void => {
-    },
-    BlockFlipY: (): void => {
-    },
-    BlockFlipZ: (): void => {
-    },
-    CopyBlueprint: (): void => {
-    },
-    PasteBlueprint: (): void => {
-    },
-    RemoveIslands: (): void => {
-    },
-    ApplyStability: (): void => {
-    },
-    TestStability: (): void => {
-    },
-    SaveBuilding: (): void => {
-    },
-    ToggleStabilityLoop: (): void => {
-    },
-    RequestBlocks: (): void => {
-    },
-    RequestSubstances: (): void => {
-    },
-    BlockIDsforSubstanceID: (substanceID: number): void => {
-    },
-    RequestBlockTags: (blockID: number): void => {
-    },
-    ChangeBlockType: (newType: number): void => {
-    },
-    OpenScreenshotShare: (): void => {
-    },
-    TakeScreenshot: (): void => {
-    },
-    CountBlocks: (): void => {
-    },
-
-    ReplaceSubstance: (block1: number, block2: number): void => {
-    },
-    ReplaceSelectedSubstance: (block1: number, block2: number): void => {
-    },
-    ReplaceShapes: (shape1: number, shape2: number): void => {
-    },
-    ReplaceSelectedShapes: (shape1: number, shape2: number): void => {
-    },
-    RotateX: (): void => {
-    },
-    RotateY: (): void => {
-    },
-    RotateZ: (): void => {
-    },
-    SnapMode: (): void => {
-    },
-    BlockTypes: (): void => {
-    },
-    LoopAbility: (hotbarIndex: number, interval: number): void => {
-    },
-    EndLoopAbility: (): void => {
-    },
-
     placedBlockCount: 0,
     blockTypes: 0,
-
-    SelectBlueprint: (index: number): void => {
-    },
-    RequestBlueprints: (): void => {
-    },
-    SaveBlueprint: (name: string): void => {
-    },
-    DownloadBlueprints: (): void => {
-    },
-    ReceiveBlueprintFromServer: (name: string, cellData: any, id: string): void => {
-    },
-    DeleteLocalBlueprint: (name: string): void => {
-    },
-
-
-    /* Announcement */
-
-    OnAnnouncement: (c: (message: string, type: number) => void): void => {
-    },
-
-    /* Plot */
-
-    OnPlotStatus: (c: (plotOwned: boolean, permissions: number, charID: string, entityID: string) => void): void => {
-    },
-
-    /* Character State Changes */
-    OnPlayerStateChanged: (c: (state: PlayerState) => void): void => {
-    },
-    OnEnemyTargetStateChanged: (c: (state: AnyEntityState) => void): void => {
-    },
-    OnFriendlyTargetStateChanged: (c: (state: AnyEntityState) => void): void => {
-    },
-
-
-    /* Character */
-
-    OnCharacterZoneChanged: (c: (zoneID: string) => void): void => {
-    },
-
-    OnCharacterCanReleaseControlChanged: (c: (canRelease: boolean) => void): void => {
-    },
-
-    /* EMOTE */
-
-    Emote: (emote: number): void => {
-    },
-
-    /* Chat */
-
-    OnBeginChat: (c: (commandMode: number, text: string) => void): void => {
-    },
-    OnChat: (c: (type: number, from: string, body: string, nick: string, iscse: boolean) => void): void => {
-    },
-    SendChat: (type: number, to: string, body: string): void => {
-    },
-    Stuck: (): void => {
-      game.sendSlashCommand('/stuck');
-    },
-    ChangeZone: (zoneID: number): void => {
-    },
-
-    /* Ability Crafting */
-
-    AbilityCreated: (abilityID: string, primaryBaseComponentID: string,
-                     secondaryBaseComponentID: string, ability: string): void => {
-    },
-    OnAbilityCreated: (callback: (abilityID: string, ability: string) => void): void => {
-    },
-
-    AbilityDeleted: (abilityID: string): void => {
-    },
-    OnAbilityDeleted: (callback: (abilityID: string) => void): void => {
-    },
-
-    RegisterAbility: (abilityID: string, primaryBaseComponentID: string, secondaryBaseComponentID: string): void => {
-    },
-    OnAbilityRegistered: (callback: (abilityID: string, cooldowns: string,
-                                     duration: number, triggerTime: number) => void): void => {
-    },
 
     /* Stats */
 
@@ -411,68 +86,39 @@ function cuAPIShim() {
     velFacing: 0,
     downCollisionAngle: 0,
     terrainCollisionAngle: 0,
+    apiVersion: 1,
+    debug: false,
+    signalRHost: 'https://api.camelotunchained.com/signalr',
 
-    /* Console */
-
-    OnConsoleText: (c: (text: string) => void): void => {
-    },
-    ConsoleCommand: (body: string): void => {
-    },
     SendSlashCommand: (command: string): void => {
       game.sendSlashCommand(command);
     },
 
-    /* Logging */
-    OnLogMessage: (c: (category: string,
-                       level: number,
-                       time: string,
-                       process: number,
-                       thread: number,
-                       message: string) => void): void => {
+    Stuck: (): void => {
+      game.sendSlashCommand('/stuck');
     },
 
-    /* Combat Logs */
-    OnCombatLogEvent: (c: (events: any) => void): void => {
+    Respawn: (id: string): void => {
+      game.selfPlayerState.respawn(id + '');
     },
 
-    /* Dev UI */
-    OnUpdateDevUI: (c: (pageID: string, rootPage: any) => void): void => {
+    ReloadUI: (name: string): void => {
+      game.reloadUI();
     },
 
-    /* Scenarios */
-    ScenarioRoundEnded: (c: (scenarioID: string, roundID: string, scenarioEnded: boolean, didWin: boolean) => void) => {
+    ReloadAllUI: (): void => {
+      game.reloadUI();
     },
 
-    /* Deployable Items */
-    StartPlacingItemByID: (numericItemDefID: number, itemInstanceID: string, extraParameters?: string): void => {},
+    OnServerConnected: (c: (isConnected: boolean) => void): number => {
+      c(false);
+      return -1;
+    },
 
-    ResetItemPlacement: (): void => {},
-
-    CommitItemPlacement: (): void => {},
-
-    CancelItemPlacement: (): void => {},
-
-    SendCommitItemRequest: (callback: (itemINstanceIDString: string, position: any, rotation: any, scale: any) => void)
-    : void => {},
-
-    /* Target */
-    RequestFriendlyTargetEntityID: (entityID: string): void => {},
-    RequestEnemyTargetEntityID: (entityID: string): void => {},
-
-    /* Keybind API */
-    OnKeybindRecorded: (callback: (keybind: Keybind) => void) => {},
-    OnAllKeybindsRequested: (callback: (bindables: any[], bindings: Binding[]) => void) => {},
-    RequestAllKeybinds: () => {},
-    StartRecordingKeybind: (button: number, alias: number) => {},
-    SetKeybind: (button: number, alias: number, boundKeyValue: number) => {},
-    CancelRecordingKeybind: () => {},
-    ClearKeybind: (button: number, alias: number) => {},
-
-    apiVersion: 1,
-    // characterID: 'unknown',
-    debug: false,
-    signalRHost: 'https://api.camelotunchained.com/signalr',
-    // shardID: 1,
+    OnInitialized: (c: () => void): number => {
+      c();
+      return -1;
+    },
   };
 }
 
@@ -492,6 +138,12 @@ export default function() {
         return obj[key];
       }
 
+      return function() {
+        const args = Array.prototype.slice.call(arguments);
+        console.warn(
+          `A method was called on cuAPI shim that has not been implemented | ${String(key)} : ${args.join(', ')}`);
+        return -1;
+      };
     },
   });
 }
