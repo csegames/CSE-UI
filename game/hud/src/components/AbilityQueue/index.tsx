a9004603c8a9ca516219794c05ea7e497b6f2e86
Ajackster <jacksonal300@gmail.com>
2018-12-19 14:41:35 -0500

    Fix duplicate ability queue items and ocassional ability queue items not going away

--- game/hud/src/components/AbilityQueue/index.tsx
+++ game/hud/src/components/AbilityQueue/index.tsx
@@ -46,55 +46,56 @@ class AbilityQueue extends React.Component<AbilityQueueProps, AbilityQueueState>
 
   private handleAbilityQueueEvent = (ability: AbilityButtonInfo) => {
     const queuedAbilities = { ...this.state.queuedAbilities };
-    const tracks = this.getTracks(ability.track);
-    tracks.forEach((track) => {
-      const activeOrQueued = ability.status & AbilityButtonState.Queued ||
-      ability.status & AbilityButtonState.Preparation ||
-      ability.status & AbilityButtonState.Held;
+    const track = this.getTrack(ability.track);
+    const activeOrQueued = ability.status & AbilityButtonState.Queued ||
+    ability.status & AbilityButtonState.Preparation ||
+    ability.status & AbilityButtonState.Held;
 
-      if (activeOrQueued) {
-        // Add active or queued ability to the AbilityQueue list
-        const abilityIndex = findIndex(queuedAbilities[track], queuedAbility => queuedAbility.id === ability.id);
-        if (abilityIndex > -1) {
-          queuedAbilities[track][abilityIndex] = ability;
-        } else {
-          queuedAbilities[track] = queuedAbilities[track] ? [...queuedAbilities[track], ability] : [ability];
-        }
-      } else if (queuedAbilities[track]) {
-        // Ability is no longer active or queued, remove from the AbilityQueue list
-        queuedAbilities[track] = queuedAbilities[track].filter(queuedAbility => queuedAbility.id !== ability.id);
-        if (queuedAbilities[track].length === 0) {
-          delete queuedAbilities[track];
-        }
+    if (activeOrQueued) {
+      // Add active or queued ability to the AbilityQueue list
+      const abilityIndex = findIndex(queuedAbilities[track], queuedAbility => queuedAbility.id === ability.id);
+      if (abilityIndex > -1) {
+        queuedAbilities[track][abilityIndex] = ability;
+      } else {
+        queuedAbilities[track] = queuedAbilities[track] ? [...queuedAbilities[track], ability] : [ability];
       }
-    });
+    } else {
+      // Ability is no longer active or queued, remove from the AbilityQueue list
+      Object.keys(queuedAbilities).forEach((track) => {
+        if (queuedAbilities[track].find(queuedAbility => queuedAbility.id === ability.id)) {
+          queuedAbilities[track] = queuedAbilities[track].filter(queuedAbility => queuedAbility.id !== ability.id);
+          if (queuedAbilities[track].length === 0) {
+            delete queuedAbilities[track];
+          }
+        }
+      });
+    }
+
     this.setState({ queuedAbilities });
   }
 
-  private getTracks = (track: AbilityTrack) => {
-    let tracks: string[] = [];
+  private getTrack = (track: AbilityTrack) => {
+    if (track & AbilityTrack.BothWeapons) {
+      return AbilityTrack[AbilityTrack.BothWeapons];
+    }
 
     if (track & AbilityTrack.PrimaryWeapon) {
-      tracks = [...tracks, 'PrimaryWeapon'];
+      return AbilityTrack[AbilityTrack.PrimaryWeapon];
     }
 
     if (track & AbilityTrack.SecondaryWeapon) {
-      tracks = [...tracks, 'SecondaryWeapon'];
+      return AbilityTrack[AbilityTrack.SecondaryWeapon];
     }
 
     if (track & AbilityTrack.Voice) {
-      tracks = [...tracks, 'Voice'];
+      return AbilityTrack[AbilityTrack.Voice];
     }
 
     if (track & AbilityTrack.Mind) {
-      tracks = [...tracks, 'Mind'];
+      return AbilityTrack[AbilityTrack.Mind];
     }
 
-    if (track === 0) {
-      tracks = [...tracks, 'None'];
-    }
-
-    return tracks;
+    return AbilityTrack[AbilityTrack.None];
   }
 }
 
