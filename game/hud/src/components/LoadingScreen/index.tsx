6c179afaf2dc784936be641dbeb3b376dfd41060
Ajackster <jacksonal300@gmail.com>
2018-12-19 10:33:07 -0500

    Fix loading screen showing up at bottom of screen following initial load

--- game/hud/src/components/LoadingScreen/index.tsx
+++ game/hud/src/components/LoadingScreen/index.tsx
@@ -12,6 +12,10 @@ import { ProgressBar } from './ProgressBar';
 
 const Container = styled('div')`
   position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   z-index: 9999;
