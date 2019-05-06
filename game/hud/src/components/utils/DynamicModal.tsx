2b1f616b24fe2f27d8351b7bfb1b2cfad95072c2
Ajackster <jacksonal300@gmail.com>
2019-05-06 16:39:05 -0400

    Fix DevUIs pushing FixedModal off the screen

--- game/hud/src/components/utils/DynamicModal.tsx
+++ game/hud/src/components/utils/DynamicModal.tsx
@@ -11,6 +11,10 @@ import { Modal } from 'shared/Modal';
 
 const Container = styled('div')`
   position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
   width: 100vw;
   height: 100vh;
   display: flex;
