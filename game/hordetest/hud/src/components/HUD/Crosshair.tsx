354ab527d76e7f1a579d709ce5e6394cf253fb76
Ajackster <jacksonal300@gmail.com>
2019-10-04 12:13:25 -0400

    Make crosshair more visible against snow

--- game/hordetest/hud/src/components/HUD/Crosshair.tsx
+++ game/hordetest/hud/src/components/HUD/Crosshair.tsx
@@ -8,8 +8,8 @@ import React from 'react';
 import { styled } from '@csegames/linaria/react';
 
 const CrosshairImage = styled.img`
-  width: 102px;
-  height: 102px;
+  width: 45px;
+  height: 45px;
 `;
 
 export function Crosshair() {
