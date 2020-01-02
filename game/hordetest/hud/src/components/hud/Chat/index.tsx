b90c12c2ea7c037bf4d56ecce7b1e1d7f392138f
Ajackster <jacksonal300@gmail.com>
2020-01-02 12:14:57 -0500

    Fix chat pushing DevUIs away

--- game/hordetest/hud/src/components/hud/Chat/index.tsx
+++ game/hordetest/hud/src/components/hud/Chat/index.tsx
@@ -11,6 +11,10 @@ import { Pane } from './views/Pane';
 
 const Screen = styled.div`
   position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   pointer-events: none !important;
