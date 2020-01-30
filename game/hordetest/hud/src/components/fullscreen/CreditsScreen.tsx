194c83da7b75add5b56b8c71d1d261bb74d2b803
jamkoo <jamkoo@gmail.com>
2020-01-30 17:21:34 -0500

    Content for Credits

--- game/hordetest/hud/src/components/fullscreen/CreditsScreen.tsx
+++ game/hordetest/hud/src/components/fullscreen/CreditsScreen.tsx
@@ -16,13 +16,42 @@ const Container = styled.div`
   right: 0;
   height: 100vh;
   width: 100vw;
-  background: url(../images/fullscreen/fullscreen-credits-bg.jpg);
-  background-size: cover;
-  animation-fill-mode: forwards;
-  animation-name: background-fading-in ;
-  animation-duration: 50s;
-  overflow: hidden;
+  background: black;
 `;
+
+const ContainerBg = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  &:before{
+    content: '';
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    background: url(../images/fullscreen/fullscreen-credits-bg.jpg);
+    background-size: cover;
+    animation-fill-mode: forwards;
+    animation-name: background-zoom-in ;
+    animation-duration: 200s;
+  }
+
+  @keyframes background-zoom-in {
+    from {
+      opacity: 0.4;
+      transform: scale(1)
+    }
+    to {
+      opacity: 1;
+      transform: scale(1.3)
+    }
+  }
+`;
+
 
 const Ray = styled.div`
   position: absolute;
@@ -30,7 +59,7 @@ const Ray = styled.div`
   animation-iteration-count: infinite;
 
   &.ray1 {
-    width:15%;
+    width:50%;
     height:100%;
     transform:  translate(350%, -50%) rotate(35deg);
     background:radial-gradient(rgba(84, 250, 255, 0.53) 0%, transparent 70%);
@@ -49,7 +78,7 @@ const Ray = styled.div`
   &.ray3 {
     width:100%;
     height:100%;
-    transform:  translate(0%, -50%);
+    transform:  translate(0%, -30%);
     filter: hue-rotate(-30deg);
     background:radial-gradient(rgba(84, 139, 255, 0.53) 0%, transparent 70%);
     animation-duration: 6s;
@@ -58,7 +87,7 @@ const Ray = styled.div`
   &.ray4 {
     width:100%;
     height:100%;
-    transform:  translate(45%, -50%);
+    transform:  translate(45%, -20%);
     filter: hue-rotate(-35deg);
     background:radial-gradient(rgba(84, 255, 205, 0.53) 0%, transparent 70%);
     animation-duration: 7s;
@@ -85,12 +114,27 @@ const CreditsText = styled.div`
   animation-timing-function: linear;
   animation-fill-mode: forwards;
   animation-name: scroll-text;
-  animation-duration: 30s;
+  animation-duration: 350s;
 
   @keyframes scroll-text {
     0% {transform: translate(20%, -100%)}
     100% {transform: translate(20%, 300%)}
   }
+`;
+
+const SubText = styled.div`
+  font-family: Colus;
+  color: white;
+  font-size: 0.8em;
+  margin-top: 100px;
+  opacity: 0.8;
+`;
+
+const SubTitle = styled.div`
+  font-family: Colus;
+  color: white;
+  margin-bottom: 100px;
+  opacity: 0.9;
 `;
 
 const Item = styled.div`
@@ -104,6 +148,7 @@ const Role = styled.div`
 
 const HideButtonPosition = styled.div`
   position: fixed;
+  z-index: 1;
   bottom: 20px;
   right: 20px;
 `;
@@ -119,35 +164,199 @@ interface CreditPerson {
 const people: CreditPerson[] = [
   {
     name: 'Andrew Jackson',
-    role: 'UI Engineer',
+    role: 'Frontend UI Engineer',
   },
   {
-    name: 'SAndrew Jackson',
-    role: 'UI Engineer',
+    name: 'Andrew Meggs',
+    role: 'CTO/Lead Software Architect',
   },
   {
-    name: 'TAndrew Jackson',
-    role: 'UI Engineer',
+    name: 'Anna Luu',
+    role: 'Director of HR',
   },
   {
-    name: 'BAndrew Jackson',
-    role: 'UI Engineer',
+    name: 'Anthony Sermania',
+    role: 'Engineer',
   },
   {
-    name: 'QAndrew Jackson',
-    role: 'UI Engineer',
+    name: 'Ben Pielstick',
+    role: 'Co-Lead Designer',
   },
   {
-    name: 'OAndrew Jackson',
-    role: 'UI Engineer',
+    name: 'Brian Ward',
+    role: 'Senior OPS/QA/CM',
   },
   {
-    name: 'LAndrew Jackson',
-    role: 'UI Engineer',
+    name: 'Bull Durham',
+    role: 'Senior Tools Engineer',
   },
   {
-    name: 'MAndrew Jackson',
-    role: 'UI Engineer',
+    name: 'Brys Sepulveda',
+    role: 'Senior Server Engineer',
+  },
+  {
+    name: 'Charles Ribemont',
+    role: 'Senior Web Admin',
+  },
+  {
+    name: 'Cheyne Anderson',
+    role: 'Client and Pipeline Engineer',
+  },
+  {
+    name: 'Christina Carr',
+    role: 'Lead Gameplay Engineer',
+  },
+  {
+    name: 'Colin Sipherd',
+    role: 'Senior Networking Engineer',
+  },
+  {
+    name: 'Daniel Beck ',
+    role: 'Lead Audio Designer/Producer',
+  },
+  {
+    name: 'Elvin Hernandez',
+    role: 'Illustrator',
+  },
+  {
+    name: 'George Davison',
+    role: 'Seattle Studio Director/Engineering Director ',
+  },
+  {
+    name: 'Harrison Rea',
+    role: 'Client Engineer',
+  },
+  {
+    name: 'James Brown',
+    role: 'Senior UI/Web Engineer',
+  },
+  {
+    name: 'James Koo',
+    role: 'Lead UI Artist',
+  },
+  {
+    name: 'Joe Janca',
+    role: 'Lead Animator',
+  },
+  {
+    name: 'Jon Young',
+    role: 'Lead Modeler',
+  },
+  {
+    name: 'Joseph Burrage',
+    role: 'Modeler/Material Artist',
+  },
+  {
+    name: 'Judd Cohen',
+    role: 'Gameplay Engineer',
+  },
+  {
+    name: 'Kara Stover',
+    role: 'Concept Artist',
+  },
+  {
+    name: 'Lee Wilson',
+    role: 'Senior Engineer',
+  },
+  {
+    name: 'Mark Chae',
+    role: 'Server Ops Engineer',
+  },
+  {
+    name: 'Mark Jacobs',
+    role: 'CEO/Creative Director',
+  },
+  {
+    name: 'Matt Meehan',
+    role: 'Senior Engineer',
+  },
+  {
+    name: 'Matthew Lauritzen',
+    role: 'Senior Gameplay Engineer',
+  },
+  {
+    name: 'Max Porter',
+    role: 'Co-writer/Editor',
+  },
+  {
+    name: 'Michael Jacobs',
+    role: 'Lead QA Tester',
+  },
+  {
+    name: 'Michelle Davies',
+    role: 'Art Director/Lead Concept Artist',
+  },
+  {
+    name: 'Mike Crossmire',
+    role: 'Senior VFX Artist',
+  },
+  {
+    name: 'Mike Dickheiser',
+    role: 'Senior Engineer',
+  },
+  {
+    name: 'Mike Waver',
+    role: 'Modeler',
+  },
+  {
+    name: 'Rob Argue',
+    role: 'Senior Server Engineer',
+  },
+  {
+    name: 'Samantha Ng',
+    role: 'Producer',
+  },
+  {
+    name: 'Sandra Pavulaan',
+    role: 'Animator',
+  },
+  {
+    name: 'Scott Trolan',
+    role: 'Animator',
+  },
+  {
+    name: 'Sierra Boyette',
+    role: 'Modeler',
+  },
+  {
+    name: 'Tim Mills',
+    role: 'Lead DevOps/Senior Engineer',
+  },
+  {
+    name: 'Tina Fulton',
+    role: 'Concept Artist',
+  },
+  {
+    name: 'Tyler Rockwell',
+    role: 'Environmental artist/Content Manager',
+  },
+
+]
+
+interface CreditSubPerson {
+  name: string;
+  role: string;
+}
+const peopleLeft: CreditSubPerson[] = [
+  {
+    name: 'Andrew Phelan',
+    role: 'Modeler',
+  },
+   {
+    name: 'Ash Kain',
+    role: 'Producer',
+  },
+   {
+    name: 'Caleb Fisher',
+    role: 'Gameplay Engineer',
+  },
+   {
+    name: 'Jon Farinelli',
+    role: 'Producer',
+  },
+   {
+    name: 'Marc Hernandez',
+    role: 'Senior Server Engineer',
   },
 ]
 
@@ -158,25 +367,38 @@ export function CreditsScreen(props: Props) {
 
   return (
     <Container>
-      <Ray className="ray1"></Ray>
-      <Ray className="ray2"></Ray>
-      <Ray className="ray3"></Ray>
-      <Ray className="ray4"></Ray>
-
-      <CreditsText>
-        {people.sort((a, b) => b.name.localeCompare(a.name)).map((person) => {
-          return (
-            <Item>
-              {person.name}
-              <Role>{person.role}</Role>
-            </Item>
-          );
-        })}
-      </CreditsText>
-
       <HideButtonPosition>
         <ActionButton onClick={hideCreditsScreen}>Hide</ActionButton>
       </HideButtonPosition>
+      <ContainerBg>
+        <Ray className="ray1"></Ray>
+        <Ray className="ray2"></Ray>
+        <Ray className="ray3"></Ray>
+        <Ray className="ray4"></Ray>
+
+        <CreditsText>
+          <SubText>
+            {peopleLeft.sort((a, b) => b.name.localeCompare(a.name)).map((peopleLeft) => {
+              return (
+                <Item>
+                  {peopleLeft.name}
+                  <Role>{peopleLeft.role}</Role>
+                </Item>
+              );
+            })}
+            <SubTitle>Those who started, but didn't complete the journey</SubTitle>
+          </SubText>
+          {people.sort((a, b) => b.name.localeCompare(a.name)).map((person) => {
+            return (
+              <Item>
+                {person.name}
+                <Role>{person.role}</Role>
+              </Item>
+            );
+          })}
+        </CreditsText>
+      </ContainerBg>
+
     </Container>
   );
 }
