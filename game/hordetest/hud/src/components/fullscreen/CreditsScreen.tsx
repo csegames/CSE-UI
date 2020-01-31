6c1be7c4f18f9bed47470404ec19662376669f91
jamkoo <jamkoo@gmail.com>
2020-01-31 12:16:32 -0500

    credit: names and animation

--- game/hordetest/hud/src/components/fullscreen/CreditsScreen.tsx
+++ game/hordetest/hud/src/components/fullscreen/CreditsScreen.tsx
@@ -43,11 +43,11 @@ const ContainerBg = styled.div`
   @keyframes background-zoom-in {
     from {
       opacity: 0.4;
-      transform: scale(1)
+      transform: scale(1) translate(0%, 0%) ;
     }
     to {
       opacity: 1;
-      transform: scale(1.3)
+      transform: scale(1.5) translate(3%, 2%);
     }
   }
 `;
@@ -110,14 +110,14 @@ const CreditsText = styled.div`
   font-family: Colus;
   color: white;
   font-size: 24px;
-  transform: translate(20%, -90%);
+  transform: translate(20%, -103%);
   animation-timing-function: linear;
   animation-fill-mode: forwards;
   animation-name: scroll-text;
   animation-duration: 350s;
 
   @keyframes scroll-text {
-    0% {transform: translate(20%, -100%)}
+    0% {transform: translate(20%, -103%)}
     100% {transform: translate(20%, 300%)}
   }
 `;
@@ -151,6 +151,105 @@ const HideButtonPosition = styled.div`
   z-index: 1;
   bottom: 20px;
   right: 20px;
+`;
+
+const Rune = styled.div`
+  font-family: fs-icons;
+  position: fixed;
+  animation: rune-rising 5s infinite;
+  opacity: 0;
+  transform: translate(0%, 0%);
+  font-size: 40px;
+  color: white;
+  text-shadow: 0px 0px 15px rgb(94, 78, 226);
+
+  &.rune1 {
+    top: 20%;
+    left: 10%;
+    animation-delay: 5s;
+    animation-duration: 15s;
+    font-size: 4em;
+  }
+
+  &.rune2 {
+    top: 100%;
+    left: 40%;
+    animation-delay: 5s;
+    animation-duration: 5s;
+    font-size: 2em;
+  }
+
+  &.rune3 {
+    top: 60%;
+    left: 60%;
+    animation-delay: 9s;
+    animation-duration: 7s;
+    font-size: 3em;
+  }
+
+  &.rune4 {
+    top: 20%;
+    left: 80%;
+    animation-delay: 13s;
+    animation-duration: 9s;
+    font-size: 4em;
+  }
+
+  &.rune5 {
+    top: 90%;
+    left: 90%;
+    animation-delay: 18s;
+    animation-duration: 8s;
+    font-size: 5em;
+  }
+
+  &.rune6 {
+    top: 100%;
+    left: 10%;
+    animation-delay: 6s;
+    animation-duration: 10s;
+    font-size: 6em;
+  }
+
+  &.rune7 {
+    top: 95%;
+    left: 50%;
+    animation-delay: 5s;
+    animation-duration: 13s;
+    font-size: 7em;
+  }
+
+  &.rune8 {
+    top: 100%;
+    left: 70%;
+    animation-delay: 9s;
+    animation-duration: 18s;
+    font-size: 8em;
+  }
+
+  &.rune9 {
+    top: 90%;
+    left: 80%;
+    animation-delay: 13s;
+    animation-duration: 16s;
+    font-size: 9em;
+  }
+
+  @keyframes rune-rising {
+    0% {
+      opacity: 0;
+      transform: translate(0%, 0%);
+    }
+    30% {
+      opacity: 0.8;
+    }
+    80%{
+      opacity: 0;
+    }
+    100% {
+      transform: translate(0%, -400%);
+    }
+  }
 `;
 
 export interface Props {
@@ -360,45 +459,57 @@ const peopleLeft: CreditSubPerson[] = [
   },
 ]
 
-export function CreditsScreen(props: Props) {
-  function hideCreditsScreen() {
-    game.trigger('hide-credits-screen');
-  }
+export class CreditsScreen extends React.Component<Props> {
+  public render() {
+    return (
+      <Container>
+        <HideButtonPosition>
+          <ActionButton onClick={this.hideCreditsScreen}>Hide</ActionButton>
+        </HideButtonPosition>
+        <ContainerBg>
+          <Ray className="ray1"></Ray>
+          <Ray className="ray2"></Ray>
+          <Ray className="ray3"></Ray>
+          <Ray className="ray4"></Ray>
 
-  return (
-    <Container>
-      <HideButtonPosition>
-        <ActionButton onClick={hideCreditsScreen}>Hide</ActionButton>
-      </HideButtonPosition>
-      <ContainerBg>
-        <Ray className="ray1"></Ray>
-        <Ray className="ray2"></Ray>
-        <Ray className="ray3"></Ray>
-        <Ray className="ray4"></Ray>
-
-        <CreditsText>
-          <SubText>
-            {peopleLeft.sort((a, b) => b.name.localeCompare(a.name)).map((peopleLeft) => {
+          <CreditsText>
+            <SubText>
+              {peopleLeft.sort((a, b) => b.name.localeCompare(a.name)).map((peopleLeft) => {
+                return (
+                  <Item>
+                    {peopleLeft.name}
+                    <Role>{peopleLeft.role}</Role>
+                  </Item>
+                );
+              })}
+              <SubTitle>Those who started, but didn't complete the journey</SubTitle>
+            </SubText>
+            {people.sort((a, b) => b.name.localeCompare(a.name)).map((person) => {
               return (
                 <Item>
-                  {peopleLeft.name}
-                  <Role>{peopleLeft.role}</Role>
+                  {person.name}
+                  <Role>{person.role}</Role>
                 </Item>
               );
             })}
-            <SubTitle>Those who started, but didn't complete the journey</SubTitle>
-          </SubText>
-          {people.sort((a, b) => b.name.localeCompare(a.name)).map((person) => {
-            return (
-              <Item>
-                {person.name}
-                <Role>{person.role}</Role>
-              </Item>
-            );
-          })}
-        </CreditsText>
-      </ContainerBg>
+          </CreditsText>
 
-    </Container>
-  );
+          <Rune className='rune1'>1</Rune>
+          <Rune className='rune2'>2</Rune>
+          <Rune className='rune3'>3</Rune>
+          <Rune className='rune4'>4</Rune>
+          <Rune className='rune5'>5</Rune>
+          <Rune className='rune6'>6</Rune>
+          <Rune className='rune7'>7</Rune>
+          <Rune className='rune8'>8</Rune>
+          <Rune className='rune9'>9</Rune>
+        </ContainerBg>
+
+      </Container>
+    );
+  }
+
+  private hideCreditsScreen = () => {
+    game.trigger('hide-credits-screen');
+  }
 }
